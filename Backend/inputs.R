9f0fa17a1bfc1410fffc17cea4f3c131a0d3f5aa
Darren Yan <darrenyankj@gmail.com>
2024-04-06 23:08:01 +0800

    dummy for AR model

--- Backend/inputs.R
+++ Backend/inputs.R
@@ -1,5 +1,5 @@
 example_startq = "1980 Q4"
-example_endq = "2014 Q4"
+example_endq = "2020 Q4"
 example_startyq = as.yearqtr(example_startq)
 example_endyq = as.yearqtr(example_endq)
 
@@ -12,3 +12,25 @@ end_y = as.numeric(year(example_endyq))
 
 start_q = as.numeric(quarter(example_startyq))
 end_q = as.numeric(quarter(example_endyq))
+
+covid = c("2020 Q2", "2020 Q3")
+covid_start = as.yearqtr(covid[1])
+covid_end = as.yearqtr(covid[2])
+covid_dummy = rep(0, (example_endyq - example_startyq) * 4 + 1)
+
+# Timeframe cannot start from during covid or after
+# Dummy if timeframe ends on 2020 Q2, start of covid
+if (example_startyq <= covid_start & example_endyq == covid_start){
+  index = (covid_start - example_startyq) * 4 + 1
+  covid_dummy[index] = 1
+}
+
+# Dummy if timeframe includes all of covid
+if (example_startyq <= covid_start & example_endyq >= covid_end){
+  index = (covid_start - example_startyq) * 4 + 1
+  covid_dummy[index] = 1
+  covid_dummy[index + 1] = 1
+}
+
+
+
