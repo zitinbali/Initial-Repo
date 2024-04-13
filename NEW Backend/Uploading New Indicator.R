c32e89d788d64a6ba3c2a96509d04a7bd396a14f
zitinbali <balizitin@gmail.com>
2024-04-13 22:03:18 +0800

    code for new indicator that's uploaded

---
+++ NEW Backend/Uploading New Indicator.R
@@ -0,0 +1,24 @@
+source("ADL Data.R")
+source("ADL Functions.R")
+
+#########################
+# new indicator added
+#########################
+
+new_indicator <- read_excel("INSERT FILE NAME HERE", 
+                            col_names = c("Date", "Spread")) %>% 
+  mutate(Date = as.yearqtr(Date), 
+         Spread = as.numeric(Spread))
+
+new_indicator <- ADL_splice(new_indicator, example_startyq, example_endyq)
+
+new_indicator_ts <- ts(new_indicator$Spread, 
+               start = c(start_y, start_q), 
+               end = c(end_y, end_q), 
+               frequency = 4)
+
+# after this, just call ADL_predict_all like you would for any other indicator
+
+ADL_predict_all(GDPGrowth_ts, new_indicator_ts, example_startq,
+                example_endq, example_fhorizon, covid_dummy)
+
